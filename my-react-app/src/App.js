cdce0f8c4e36df67e46e756f8d83b2a20b6ef9f8
lelani82 <lelaniparker@hotmail.com>
2020-03-19 20:03:02 +1000

    setState

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -3,19 +3,38 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  // adding state:
   state = {
     persons: [
       { name: 'Max', age: 28 },
       { name: 'Steph', age: 26 },
       { name: 'Ed', age: 29 }
-    ]
+    ],
+    anotherState: 'This state wont change'
   }
 
+  // adding event handler:
+  switchNameHandler = () => {
+    // console.log('Button was clicked');
+    // DO NOT MANIPULATE STATE DIRECTLY: this.state.persons[0].name = "Maximilian";
+
+    // setState will update current state object with this new object:
+    this.setState({
+      persons: [
+        { name: 'Maximiliam', age: 28 },
+        { name: 'Steph', age: 26 },
+        { name: 'Ed', age: 30 }
+      ]
+    })
+  }
+
+  // render method:
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>Have fun creating apps with React!</p>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person name={this.state.persons[0].name}
           age={this.state.persons[0].age} />
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
