99caf997a86be4f1f309f9f713961a6028ab0207
lelani82 <lelaniparker@hotmail.com>
2020-03-19 16:51:02 +1000

    state

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hi, I'm a React App</h1>
-      <p>Have fun creating apps with React!</p>
-      <Person />
-      <Person />
-      <Person />
-    </div>
-  );
+class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Steph', age: 26 },
+      { name: 'Ed', age: 29 }
+    ]
+  }
 
-  // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Hello there'))
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p>Have fun creating apps with React!</p>
+        <Person name={this.state.persons[0].name}
+          age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+      </div>
+    );
+
+    // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Hello there'))
+  }
 }
 
 export default App;
