384e23e469937c5b20e236c21bf1a4ba554ef446
lelani82 <lelaniparker@hotmail.com>
2020-03-21 16:24:24 +1000

    added header and footer

---
+++ portfolio/src/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const header = () => {
+    return (
+        <div>
+            <a href="index.html" title="Go to Home page">Home</a>
+            <a href="about.html" title="Go to About page">About</a>
+            <a href="projects.html" title="Go to Projects page">Projects</a>
+            <a href="contact.html" title="Go to Contact page">Contact</a>
+        </div>
+    )
+}
+
+export default header;
