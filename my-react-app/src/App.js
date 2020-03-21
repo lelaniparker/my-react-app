a222ab5f7f3ba60424935a2a5eaa7fa4c669cd7d
lelani82 <lelaniparker@hotmail.com>
2020-03-21 10:56:02 +1000

    added styling

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -28,20 +28,43 @@ class App extends Component {
     })
   }
 
+  // adding new event handler:
+  onChangeHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: 'Max', age: 28 },
+        { name: event.target.value, age: 26 },
+        { name: 'Ed', age: 29 }
+      ]
+    })
+  }
+
   // render method:
   render() {
+    //adding inline styles:
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>Have fun creating apps with React!</p>
-        <button onClick={() => this.switchNameHandler('Maximus')}>Switch Name </button>
+        <button
+          style={style}
+          onClick={() => this.switchNameHandler('Maximus')}>Switch Name </button>
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age} />
         <Person
           name={this.state.persons[1].name}
           age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this, "No just Max")}>
+          click={this.switchNameHandler.bind(this, "No just Max")}
+          changed={this.onChangeHandler}>
           My Hobbies: Racing</Person>
         <Person
           name={this.state.persons[2].name}
